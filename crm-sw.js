1ef667506dec38d210b7f323c3e601c489e0acee
LeoDemonL13 <leondemoniaco16@gmail.com>
2026-08-24 16:22:26 -0600

    Leo

--- crm-sw.js
+++ crm-sw.js
@@ -1,4 +1,4 @@
 'use strict';
-const RELEASE='124';
+const RELEASE='125';
 self.addEventListener('install',()=>{self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{try{const keys=await caches.keys();await Promise.all(keys.filter(key=>key.startsWith('skilled-crm-')).map(key=>caches.delete(key)))}catch(_){}try{await self.registration.unregister()}catch(_){}try{await self.clients.claim()}catch(_){}})())});
